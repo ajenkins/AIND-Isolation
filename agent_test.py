037db626bffde85830daef54408ec9b7c9e9576d
AJ Jenkins <ajenkins@kyruus.com>
2017-10-04 22:38:09 -0400

    Fixed tests

--- agent_test.py
+++ agent_test.py
@@ -7,15 +7,13 @@ import unittest
 
 import isolation
 import game_agent
+from sample_players import null_score
 
 from importlib import reload
 
 
 class IsolationTest(unittest.TestCase):
     """Unit tests for isolation agents"""
-
-    def _score_function(self, game, player):
-        return game.utility(player)
 
     def _time_left(self):
         return 1000
@@ -27,31 +25,31 @@ class IsolationTest(unittest.TestCase):
         self.game = isolation.Board(self.player1, self.player2, width=3, height=3)
 
     def test_minimax_best_move(self):
-        player = game_agent.MinimaxPlayer(search_depth=10, score_fn=self._score_function)
+        player = game_agent.MinimaxPlayer(search_depth=10, score_fn=null_score)
         self.assertEqual((0, 0), player.get_move(self.game, self._time_left))
 
     def test_minimax_first_taken(self):
-        player = game_agent.MinimaxPlayer(search_depth=10, score_fn=self._score_function)
+        player = game_agent.MinimaxPlayer(search_depth=10, score_fn=null_score)
         self.game.apply_move((0, 0))
         self.assertEqual((1, 0), player.get_move(self.game, self._time_left))
 
     def test_minimax_boxed_in(self):
-        player = game_agent.MinimaxPlayer(search_depth=10, score_fn=self._score_function)
+        player = game_agent.MinimaxPlayer(search_depth=10, score_fn=null_score)
         self.game.apply_move((0, 0))
         self.game.apply_move((0, 1))
         self.assertEqual((2, 1), player.get_move(self.game, self._time_left))
 
     def test_alphabeta_best_move(self):
-        player = game_agent.AlphaBetaPlayer(search_depth=10, score_fn=self._score_function)
+        player = game_agent.AlphaBetaPlayer(search_depth=10, score_fn=null_score)
         self.assertEqual((0, 0), player.get_move(self.game, self._time_left))
 
     def test_alphabeta_first_taken(self):
-        player = game_agent.AlphaBetaPlayer(search_depth=10, score_fn=self._score_function)
+        player = game_agent.AlphaBetaPlayer(search_depth=10, score_fn=null_score)
         self.game.apply_move((0, 0))
         self.assertEqual((1, 0), player.get_move(self.game, self._time_left))
 
     def test_alphabeta_boxed_in(self):
-        player = game_agent.AlphaBetaPlayer(search_depth=10, score_fn=self._score_function)
+        player = game_agent.AlphaBetaPlayer(search_depth=10, score_fn=null_score)
         self.game.apply_move((0, 0))
         self.game.apply_move((0, 1))
         self.assertEqual((2, 1), player.get_move(self.game, self._time_left))
